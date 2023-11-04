c58e35c525e282e025dc5dc055ad3c6cc1ebdf7f
rahelanna <113769125+rahelanna@users.noreply.github.com>
2023-11-04 12:34:53 +0200

    first exercise

---
+++ practice01/variables.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main() {
+    
+    int age = 3;
+    char name[] = "Carlos";
+
+    printf("Hi, %s.\n", name); 
+    printf("You are %d years old", age);
+    
+    return 0;
+}
